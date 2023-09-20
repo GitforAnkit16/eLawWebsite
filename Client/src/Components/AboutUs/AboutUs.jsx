cfd3466a9878ce05bd5a1367e8977b076864f826
preetamburla <145477066+preetamburla@users.noreply.github.com>
2023-09-20 21:54:04 +0530

    Create AboutUs.jsx

---
+++ Client/src/Components/AboutUs/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./AboutUs.css"
+function AboutUs(){
+    return(
+        <section className='AboutUs'>
+            <div className='contu'>
+                <h1>About Us</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde et facilis earum ratione harum aliquam, molestiae voluptate impedit vel voluptatibus eos rem. Minima odio eum sequi, ea rerum cum sint.</p>
+                <button className='but'>Know More</button>
+            </div>
+        </section>
+    )
+}
+
+export default AboutUs
