0e5a57cb7b82f49bd03def3768daed2ccec5d094
preetamburla <145477066+preetamburla@users.noreply.github.com>
2023-09-20 21:56:26 +0530

    Update Home.jsx

--- Client/src/Pages/home/Home.jsx
+++ Client/src/Pages/home/Home.jsx
@@ -3,12 +3,14 @@ import "./Home.css"
 import Featured from '../../Components/Featured/Featured'
 import Slider from "../../Components/Slider/Slider"
 import Internships from '../../Components/Internships/Internships'
+import AboutUs from '../../Components/AboutUs/AboutUs'
 export default function Home() {
   return (
     <div className="home">
       <Featured />
       <Slider />
       <Internships />
+      <AboutUs />
     </div>
   )
 }
