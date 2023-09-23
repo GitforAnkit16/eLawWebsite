c29881444a6961698e60c6889430a6441e2a89f5
preetamburla <145477066+preetamburla@users.noreply.github.com>
2023-09-24 01:26:48 +0530

    Update notaries.jsx

--- Client/src/Pages/notaries/notaries.jsx
+++ Client/src/Pages/notaries/notaries.jsx
@@ -1,9 +1,162 @@
-import React from 'react'
-import "./notaries.css"
-function notaries() {
+import React, { useState } from 'react';
+import './notaries.css';
+
+function Notaries() {
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(activeCategory === category ? null : category);
+  };
+
+  const med = [
+    "Notary Public",
+"Civil Law Notary",
+"Common Law Notary",
+"Notary at Sea (Notary Mariner)",
+"Mobile Notary",
+"Online Notary (eNotary)",
+"Remote Online Notary (RON)",
+"Notary Signing Agent (NSA)",
+"Specialized Notary (e.g., Immigration Notary, Real Estate Notary)",
+"Corporate Notary (In-House Notary)",
+"Notary at Large (Notary-at-Large)",
+"Notary at the Embassy (Consular Notary)",
+"International Notary (Notary for International Documents)",
+"Notary for the Deaf and Hard of Hearing (Deaf Notary)"
+
+
+
+  ];
+
   return (
-    <div>notaries</div>
-  )
+    <div className='Notaries'>
+      <div className='grid'>
+        {med.map((category, index) => (
+          <div
+            key={index}
+            className={`category ${activeCategory === category ? 'active' : ''}`}
+            onClick={() => handleCategoryClick(category)}
+          >
+            <span>{category}</span>
+          </div>
+        ))}
+      </div>
+      {activeCategory && (
+        <div className='profiles'>
+        <span className='header-Arbitratoryer'>Profiles for {activeCategory}</span>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Criminal Notaries</li>
+                <li>Civil Notaries</li>
+                <li>Family Notaries</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default notaries
+export default Notaries;
