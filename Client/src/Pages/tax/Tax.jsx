1adab1b2de6a00660e31ac041ab2b1f801b34a9b
preetamburla <145477066+preetamburla@users.noreply.github.com>
2023-09-24 20:09:11 +0530

    Update Tax.jsx

--- Client/src/Pages/tax/Tax.jsx
+++ Client/src/Pages/tax/Tax.jsx
@@ -1,9 +1,157 @@
-import React from 'react'
+
+import "./Tax.css"
+import React, { useState } from 'react';
 
 function Tax() {
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(activeCategory === category ? null : category);
+  };
+
+  const categorie = [
+    "GST and Other Indirect Tax",
+    "Changes in Pvt Ltd Company",
+    "Changes In Limited Liability Partnership",
+    "Mandatory Annual Filings",
+    "Labour Compliance",
+    "Accounting & Tax",
+    "Convert Your Business"
+
+
+
+  ];
+
   return (
-    <div>Tax</div>
-  )
+    <div className='Tax'>
+      <div className='grid'>
+        {categorie.map((category, index) => (
+          <div
+            key={index}
+            className={`category ${activeCategory === category ? 'active' : ''}`}
+            onClick={() => handleCategoryClick(category)}
+          >
+            <span>{category}</span>
+          </div>
+        ))}
+      </div>
+      {activeCategory && (
+        <div className='profiles'>
+        <span className='header-Arbitratoryer'>Profiles for {activeCategory}</span>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership"</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>GST and Other Indirect Tax </li>
+                <li>Changes in Pvt Ltd Company</li>
+                <li>Changes In Limited Liability Partnership</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          
+        </div>
+      )}
+    </div>
+  );
 }
+
 
 export default Tax
