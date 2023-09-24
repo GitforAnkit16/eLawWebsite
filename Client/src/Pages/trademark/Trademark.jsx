f15f4d765d39bb5765c54582da7e4154db58aa7f
preetamburla <145477066+preetamburla@users.noreply.github.com>
2023-09-24 20:12:38 +0530

    Update Trademark.jsx

--- Client/src/Pages/trademark/Trademark.jsx
+++ Client/src/Pages/trademark/Trademark.jsx
@@ -1,9 +1,164 @@
-import React from 'react'
-import "./Trademark.css"
+
+import "../tax/Tax.css"
+import React, { useState } from 'react';
+
 function Trademark() {
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(activeCategory === category ? null : category);
+  };
+
+  const categorie = [
+    "Trademark",
+    "Copyright",
+    "Infringement",
+    "Design Registeration"
+
+
+
+  ];
+
   return (
-    <div>trademark</div>
-  )
+    <div className='Tax'>
+      <div className='grid'>
+        {categorie.map((category, index) => (
+          <div
+            key={index}
+            className={`category ${activeCategory === category ? 'active' : ''}`}
+            onClick={() => handleCategoryClick(category)}
+          >
+            <span>{category}</span>
+          </div>
+        ))}
+      </div>
+      {activeCategory && (
+        <div className='profiles'>
+        <span className='header-Arbitratoryer'>Profiles for {activeCategory}</span>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+                <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          <div className='profile'>
+            <img src = "https://picsum.photos/200/300" alt="profile-dp"></img>
+            <h3>John Snow</h3>
+            <p>Specialization:-</p>
+              <ul>
+              <li>Trademark</li>
+                <li>Copyright</li>
+                 <li>Infringement</li>
+                <li>Design Registeration</li>
+              </ul>
+              <p>Location:- Bhubaneswar,Odisha,India</p>
+               <button>Book An Appointment</button> 
+          </div>
+          
+        </div>
+      )}
+    </div>
+  );
 }
+
 
 export default Trademark
